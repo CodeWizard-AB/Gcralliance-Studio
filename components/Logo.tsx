11917150fa3dd2ddbf767633f0e6bf918633f431
Anuany Argha <anunayargha@gmail.com>
2026-04-22 07:46:00 +0600

    feat: add MyLogo component and integrate it into the sanity configuration

---
+++ components/Logo.tsx
@@ -0,0 +1,8 @@
+export const MyLogo = () => (
+  <img
+    src="https://ik.imagekit.io/gcralliance/Gcralliance-logo.jpg"
+    alt="My Logo"
+    width={25}
+    height={25}
+  />
+)
--- sanity.config.ts
+++ sanity.config.ts
@@ -3,8 +3,10 @@ import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 import {structure} from './structure'
+import {MyLogo} from './components/Logo'
 
 export default defineConfig({
+  icon: () => MyLogo(),
   name: 'default',
   title: 'Gcralliance-Studio',
   basePath: '/studio',
